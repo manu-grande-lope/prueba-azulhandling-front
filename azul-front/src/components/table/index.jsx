5d4bc79f41c4966aaf1e1dab79121a21e12ad8f7
manu-grande-lope <manugrande@gmail.com>
2022-02-05 21:38:38 +0100

    problem: Tabla implementada, no funciona

--- azul-front/src/components/table/index.jsx
+++ azul-front/src/components/table/index.jsx
@@ -9,6 +9,7 @@ import Paper from '@mui/material/Paper';
 import { useEffect } from 'react';
 import { Fragment } from 'react';
 import { useState } from 'react';
+import TablePagination from '@mui/material/TablePagination';
 
 export default function DataGrab() {
 
@@ -39,65 +40,39 @@ useEffect(()=>{
 },[]);
 
   return (
-
-    <div>
-      <ul>
-      {flights.departures?.map(data => <li>{data.departure.gate}</li>)}
-      </ul>
-    </div>
-  )
-
-};
-
-
-
-// function createData(name, ATP, FlightNumber, Reg, Type) {
-//   return { name, ATP, FlightNumber, Reg, Type };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
-// export default function TableFlights() {
-//   return (
-//     <TableContainer component={Paper}>
-//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>INFO</TableCell>
-//             <TableCell>ARRIVALS</TableCell>
-//             <TableCell>DEPARTURES</TableCell>
-//           </TableRow>
-//           <TableRow>
-//             <TableCell>FLIGHTS</TableCell>
-//             <TableCell align="right">ATP</TableCell>
-//             <TableCell align="right">FlightNumber</TableCell>
-//             <TableCell align="right">Reg</TableCell>
-//             <TableCell align="right">Type</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {rows.map((row) => (
-//             <TableRow
-//               key={row.name}
-//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//             >
-//               <TableCell component="th" scope="row">
-//                 {row.name}
-//               </TableCell>
-//               <TableCell align="right">{row.ATP}</TableCell>
-//               <TableCell align="right">{row.FlightNumber}</TableCell>
-//               <TableCell align="right">{row.Reg}</TableCell>
-//               <TableCell align="right">{row.Type}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   );
-// }
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>INFO</TableCell>
+            <TableCell>ARRIVALS</TableCell>
+            <TableCell>DEPARTURES</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>FLIGHTS</TableCell>
+            <TableCell align="right">ATP</TableCell>
+            <TableCell align="right">FlightNumber</TableCell>
+            <TableCell align="right">Reg</TableCell>
+            <TableCell align="right">Type</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {flights.departures?.map((data, index) => (
+            <TableRow
+            key={index}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {data.number}
+              </TableCell>
+              <TableCell align="right">{data.airport}</TableCell>
+              <TableCell align="right">{data.callsign}</TableCell>
+              <TableCell align="right">{data.status}</TableCell>
+              <TableCell align="right">{data.airline}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
